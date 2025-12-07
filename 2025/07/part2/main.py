94b367fd76d15fec4e6d6e6a69e84727fa912eed
Ken Smith <fwxgq.oysqv@fastmail.com>
2025-12-07 15:49:31 -0800

    synced with ge

---
+++ 2025/07/part2/main.py
@@ -0,0 +1,42 @@
+class Manifold:
+    def __init__(self, s):
+        self.s = list(map(list, s.strip().split("\n")))
+        self.find_emitter()
+
+    def rows(self):
+        return len(self.s)
+
+    def __repr__(self):
+        r = []
+        for s in self.s:
+            r.append("".join(s))
+        return "\n".join(r)
+
+    def find_emitter(self):
+        for i, c in enumerate(self.s[0]):
+            if c == "S":
+                self.emitter = i
+
+    def fire(self):
+        return self.fire_helper(self.emitter, 0)
+
+    def fire_helper(self, x, y):
+        if y >= self.rows() - 1:
+            print(f"done at ({x}, {y})")
+            return 1
+
+        ny = y + 1
+        if self.s[ny][x] == "^":
+            print(f"splitting at ({x}, {y})")
+            return self.fire_helper(x-1, ny) + self.fire_helper(x+1, ny)
+
+        print(f"(continuing at ({x}, {y})")
+        return self.fire_helper(x, ny)
+
+def main():
+    with open("input") as f:
+        m = Manifold(f.read())
+        print(m.fire())
+
+if __name__ == "__main__":
+    main()
