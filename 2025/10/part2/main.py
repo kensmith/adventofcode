b72ec0bec0dc5fa3c1ad477d1cc9bfd8cc73128d
Ken Smith <fwxgq.oysqv@fastmail.com>
2025-12-10 19:37:37 -0800

    synced with ge

--- 2025/10/part2/main.py
+++ 2025/10/part2/main.py
@@ -26,18 +26,6 @@ class Machine:
         self.joltages = parse_joltages(tokens[-1])
         self.buttons, self.button_map = parse_buttons(tokens[1:len(tokens)-1], len(self.joltages))
 
-    def start(self):
-        valid_seqs = []
-        for i in range(1, len(self.button_ints)):
-            for seq in list(combinations(self.button_ints, i)):
-                result = 0
-                for n in seq:
-                    result ^= n
-                if result == self.target:
-                    valid_seqs.append(seq)
-        seq_lengths = sorted(list(map(len, valid_seqs)))
-        return seq_lengths[0]
-
     def adjust(self):
         # TODO use buttons to increment via list(map(lambda a:a[0]+a[1],zip(x,y)))
         # and use button_map to find candidate presses which affect a given button
